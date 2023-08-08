3ae3b4a622609b65679d25e6b5a4f006bcfcb7e1
choseGG <1249615128@qq.com>
2023-08-08 16:45:31 +0800

    1645

--- exercises/quiz3.rs
+++ exercises/quiz3.rs
@@ -16,16 +16,19 @@
 //
 // Execute `rustlings hint quiz3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+
 
 pub struct ReportCard {
-    pub grade: f32,
+    pub grade: T,
     pub student_name: String,
     pub student_age: u8,
 }
 
 impl ReportCard {
-    pub fn print(&self) -> String {
+    pub fn print(&self) -> String 
+    where 
+        T: std::fmt::Display,    
+    {
         format!("{} ({}) - achieved a grade of {}",
             &self.student_name, &self.student_age, &self.grade)
     }
