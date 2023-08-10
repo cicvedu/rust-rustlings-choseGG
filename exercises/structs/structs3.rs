d1a52fd623658ed4f21bb6c35faac6695d4d6eca
choseGG <1249615128@qq.com>
2023-08-10 15:33:11 +0800

    1533

--- exercises/structs/structs3.rs
+++ exercises/structs/structs3.rs
@@ -29,11 +29,17 @@ impl Package {
         }
     }
 
-    fn is_international(&self) -> ??? {
-        // Something goes here...
+    fn is_international(&self) -> bool {
+        if  self.sender_country == self.recipient_country
+        {
+            false
+        }else {
+            true
+        }// Something goes here...
     }
 
-    fn get_fees(&self, cents_per_gram: i32) -> ??? {
+    fn get_fees(&self, cents_per_gram: i32) -> i32 {
+        self.weight_in_grams*cents_per_gram
         // Something goes here...
     }
 }
