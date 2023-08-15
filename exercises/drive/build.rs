b53f2380fc5f7520f7689684f42382428dcc831b
choseGG <1249615128@qq.com>
2023-08-15 17:00:53 +0800

    1700 last

--- exercises/drive/build.rs
+++ exercises/drive/build.rs
@@ -1,16 +1,4 @@
-use std::env;
-
 fn main() {
-    #[cfg(test)]
-    if let Ok(test_foo) = env::var("TEST_FOO") {
-        println!("cargo:rustc-env=TEST_FOO={}", test_foo);
-    }
-
-    #[cfg(test)]
     #[cfg(feature = "pass")]
-    if let Ok(test_feature) = env::var("TEST_FEATURE") {
-        if test_feature == "pass" {
-            println!("cargo:rustc-cfg=feature=\"pass\"");
-        }
-    }
+    println!("cargo:rustc-cfg=feature=\"pass\"");
 }
