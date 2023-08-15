f32ef13e21b6f285a871bd54770fa718e9f5fff9
choseGG <1249615128@qq.com>
2023-08-15 22:28:45 +0800

    ss

--- exercises/drive/build.rs
+++ exercises/drive/build.rs
@@ -1,8 +1,11 @@
 use std::env;
 
 fn main() {
+    // 获取环境变量的值
+    let test_foo = env::var("TEST_FOO").unwrap_or_else(|_| String::from("1234567890"));
+
     // 设置环境变量
-    println!("cargo:rustc-env=TEST_FOO=1234567890");
+    println!("cargo:rustc-env=TEST_FOO={}", test_foo);
 
     // 设置特征标识符
     #[cfg(not(feature = "pass"))]
