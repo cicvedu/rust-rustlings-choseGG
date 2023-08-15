6b47814f2af5dd63d2f9c8b219e9a0b32791bf0a
choseGG <1249615128@qq.com>
2023-08-15 18:04:32 +0800

    1804 last

--- exercises/drive/build.rs
+++ exercises/drive/build.rs
@@ -1,4 +1,10 @@
+use std::env;
+
 fn main() {
-    #[cfg(feature = "pass")]
-    println!("cargo:rustc-cfg=feature=\"pass\"");
+    // 设置环境变量
+    env::set_var("TEST_FOO", "1234567890");
+
+    // 设置特征标识符
+    #[cfg(not(feature = "pass"))]
+    println!("cargo:rustc-cfg=feature=\"not_pass\"");
 }
