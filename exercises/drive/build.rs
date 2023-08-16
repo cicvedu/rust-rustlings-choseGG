f5688dcc41bd34a1c5fa17fde82221d3c566f2d3
choseGG <1249615128@qq.com>
2023-08-16 10:24:17 +0800

    1024last

--- exercises/drive/build.rs
+++ exercises/drive/build.rs
@@ -1,13 +1,7 @@
-use std::env;
 
 fn main() {
-    // 获取环境变量的值
-    let test_foo = env::var("TEST_FOO").unwrap_or_else(|_| String::from("1234567890"));
-
-    // 设置环境变量
-    println!("cargo:rustc-env=TEST_FOO={}", test_foo);
-
-    // 设置特征标识符
-    #[cfg(not(feature = "pass"))]
-    println!("cargo:rustc-cfg=feature=\"not_pass\"");
+    println!("hello world!");
+    let timestamp = std::time::SystemTime::now().duration_since(std::time::UNIX_EPOCH).unwrap().as_secs();
+    println!("cargo:rustc-env=TEST_FOO={}", timestamp.to_string());
+    println!("cargo:rustc-cfg=feature=\"{}\"", "pass");
 }
